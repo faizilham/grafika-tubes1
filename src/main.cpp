d7538727eaa29bb7454e85263dca2981ee5d9642
unknown <adilelfahmi@gmail.com>
2014-03-31 09:10:20 +0700

    lupa ngefix main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,52 +21,26 @@ bool getch_async(char& c){
 
 int main(){
 	init_graph();
-	
-<<<<<<< HEAD
-	//draw_ellipse(10, 10, 30, 80, 255);
-	Car car;
-	Road r;
-	Quad q (-100, -100, -50, -50);
-	Transform t = createScale(0.01, 0.01);
-	
-	q.applyTransform(t);
-	
-	t = createScale(1.1, 1.1);
-	
-	ObstacleManager OM;
-	
-	char c;
-=======
 	Road r;	
 	ObstacleManager OM;
 	
 	char c = 0;
 	
 	// the game loop
->>>>>>> 197faf57e83e6be4f410afabdd12d1338ccaef05
 	while (c != 'q'){
 		cleardevice();
 		
 		// draw phase
 		r.draw();
 		car.draw();
-		OM.draw();
-<<<<<<< HEAD
-		
-		//q.draw(WHITE);
-=======
->>>>>>> 197faf57e83e6be4f410afabdd12d1338ccaef05
-		
+		OM.draw();		
+
 		// update phase
 		r.update();
 		car.update(c);
 		OM.update();
-<<<<<<< HEAD
-		//q.applyTransform(t);
+
 		c = 0;
-=======
-		
->>>>>>> 197faf57e83e6be4f410afabdd12d1338ccaef05
 		delay(100);
 		getch_async(c);
 	}
